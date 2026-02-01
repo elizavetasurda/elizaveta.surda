1293ff47cdc4189592de693d0796e96f8bfb4cd8
GG1Kot <ggkit00@mail.ru>
2026-02-01 22:03:18 +0500

    fix1

--- project c.c
+++ project c.c
@@ -32,7 +32,7 @@ double bounds[4][2] = {
 
 typedef struct {
     double x[4];  //r l ts th
-    double y;     //znachenie celevoi func(mass)
+    double y;     //znachenie celevoi func(mass+shtraf)
 } Sample;
 
 Sample dataset[MAX_DATA]; //massiv vseh experiment tochek
@@ -45,57 +45,33 @@ double rand_uniform(double a, double b)
     return a + (b - a) * ((double)rand() / RAND_MAX);
 }
 
-//celevaia func (TOLKO MASSA)
+//celevaia func + shtraf‼️
 
-double mass_function(double x[4])
+double objective(double x[4])
 {
     double R = x[0];
     double L = x[1];
     double Ts = x[2];
     double Th = x[3];
-    //volume matirial
-    double Vmat =
-        (4.0 / 3.0) * PI * (pow(R + Th, 3) - pow(R, 3)) + 
-        PI * L * (pow(R + Ts, 2) - pow(R, 2));
-    //massa
-    double mass = rho * Vmat;
-
-    return mass;
-}
-
-//shtraf otdelno
-
-double penalty_function(double x[4])
-{
-    double R = x[0];
-    double L = x[1];
-    double Ts = x[2];
-    double Th = x[3];
-
-    double penalty = 0.0;
-
     //volume sosyda
     double V = (4.0 / 3.0) * PI * pow(R, 3) + PI * R * R * L;
-
+    //volume matirial
+    double Vmat =
+        (4.0 / 3.0) * PI * (pow(R + Th, 3) - pow(R, 3)) + PI * L * (pow(R + Ts, 2) - pow(R, 2));
+    //massa
+    double mass = rho * Vmat;
     //voltage
     double sigma1 = P * R / (2.0 * Th);
     double sigma2 = P * R / Ts;
     double sigma = sigma1 > sigma2 ? sigma1 : sigma2;
-
+    //shtraf
+    double penalty = 0.0;
     if (V < Vmin)
         penalty += 1e7;
 
     if (sigma > sigma_allow)
         penalty += 1e7;
-
-    return penalty;
-}
-
-//polnaya celevaia func (mass + shtraf)
-
-double objective(double x[4])
-{
-    return mass_function(x) + penalty_function(x);
+    return mass + penalty; //imenno eto optimiziryet algoritm
 }
 
 //gaussian process 
@@ -185,10 +161,7 @@ int main()
     for (int i = 0; i < INIT_POINTS; i++)
     {
         random_point(dataset[i].x);
-
-        //GP obuchaetsya TOLKO na masse
-
-        dataset[i].y = mass_function(dataset[i].x);
+        dataset[i].y = objective(dataset[i].x); //schitaem func
     }
 
     data_count = INIT_POINTS;
@@ -197,7 +170,7 @@ int main()
 
     for (int iter = 0; iter < ITERATIONS; iter++) //iteracii Bayesian Optimization
     {
-        double best = dataset[0].y; //tekyshii min massy
+        double best = dataset[0].y; //tekyshii min
         for (int i = 1; i < data_count; i++) 
             if (dataset[i].y < best)
                 best = dataset[i].y;
@@ -223,9 +196,8 @@ int main()
             }
         }
 
-        //вычисляем реальную функцию (TOLKO MASSA dlya GP)
-
-        dataset[data_count].y = mass_function(best_candidate);
+        //вычисляем реальную функцию
+        dataset[data_count].y = objective(best_candidate);
 
         for (int k = 0; k < 4; k++)
             dataset[data_count].x[k] = best_candidate[k];
@@ -246,10 +218,7 @@ int main()
     printf("L  = %.4f m\n", dataset[best_id].x[1]);
     printf("Ts = %.4f m\n", dataset[best_id].x[2]);
     printf("Th = %.4f m\n", dataset[best_id].x[3]);
-
-    printf("Mass (pure) = %.2f kg\n", dataset[best_id].y);
-    printf("Penalty     = %.2f\n", penalty_function(dataset[best_id].x));
-    printf("Objective   = %.2f\n", objective(dataset[best_id].x));
+    printf("Mass = %.2f kg\n", dataset[best_id].y);
 
     return 0;
 }
